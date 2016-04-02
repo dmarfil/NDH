8ebd3d8421ad97ae51bd5ade6d14e49e35595321
dmarfil <dperez@negociosdehoy.com>
2016-04-02 11:07:14 -0600

    Added files via upload

---
+++ ndh.php
@@ -0,0 +1,11 @@
+<?php
+//Archivo:             ndh.php
+//Descripcion:         Archivo inicial del Framework de NDH
+//Autor:               dmarfil
+//Fecha de creación:   1/07/2012 
+//Ultima modificación: 1/07/2012 
+
+include_once(dirname(__FILE__) . "/Db/Cnx.php");        //Agregar nota
+include_once(dirname(__FILE__) . "/Db/RecordSet.php");  //Agregar nota
+include_once(dirname(__FILE__) . "/Html/Form.php");     //Agregar nota
+include_once(dirname(__FILE__) . "/Html/Table.php");     //Agregar nota
